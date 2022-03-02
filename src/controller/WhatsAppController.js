1de25e6ec0a4dbd5447ff9181e6df50156ce03ab
Guilherme Eduardo Bubliz <60118323+GuiBublitz@users.noreply.github.com>
2022-03-02 11:21:41 -0300

    Obtendo dados dos painéis e usando FormData

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -61,6 +61,24 @@ class WhatsAppController{
         Element.prototype.hasClass = function(name){
             return this.classList.contains(name);
         }
+        HTMLFormElement.prototype.getForm = function(){
+            
+            return new FormData(this);
+
+        }
+        HTMLFormElement.prototype.toJSON = function(){
+
+            let json = {};
+            
+            this.getForm().forEach((value, key)=>{
+
+                json[key] = value;
+
+            });
+
+            return json; 
+
+        }
     }
     initEvents(){
 
@@ -70,7 +88,7 @@ class WhatsAppController{
             this.el.panelEditProfile.show();
             setTimeout(()=>{
                 this.el.panelEditProfile.addClass('open');
-            }, 300);
+            },50);
 
         });
 
@@ -80,7 +98,7 @@ class WhatsAppController{
             this.el.panelAddContact.show();
             setTimeout(()=>{
                 this.el.panelAddContact.addClass('open');
-            },300);
+            },50);
 
         });
 
@@ -93,6 +111,35 @@ class WhatsAppController{
         this.el.btnClosePanelAddContact.on('click', e=>{
 
             this.el.panelAddContact.removeClass('open');
+
+        });
+
+        this.el.photoContainerEditProfile.on('click', (e)=>{
+
+            this.el.inputProfilePhoto.click();
+
+        });
+
+        this.el.inputNamePanelEditProfile.on('keypress', (e)=>{
+            
+            if(e.key === 'Enter'){
+                e.preventDefault();
+                this.el.btnSavePanelEditProfile.click();
+            }
+
+        });
+
+        this.el.btnSavePanelEditProfile.on('click',(e)=>{
+
+            console.log(this.el.inputNamePanelEditProfile.innerHTML);
+
+        });
+
+        this.el.formPanelAddContact.on('submit', (e)=>{
+
+            e.preventDefault();
+            
+            
 
         });
 
