0cdc08b7297495be9cef0638673cc1bbcfd595cc
Guilherme Eduardo Bubliz <60118323+GuiBublitz@users.noreply.github.com>
2022-03-06 11:27:20 -0300

    Importando e Exportando módulos com Webpack

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -1,4 +1,7 @@
-class WhatsAppController{
+import {Format} from './../utils/Format';
+import {CameraController} from './CameraController'; 
+
+export class WhatsAppController{
 
     constructor(){
 
@@ -391,3 +394,4 @@ class WhatsAppController{
     }
 
 }
+
